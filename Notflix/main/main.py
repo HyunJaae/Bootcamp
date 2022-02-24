7cbec5ea5a6ac0c6151e0cc7bb994c8697c6e4f1
ho-bolt <gbolt@naver.com>
2022-02-24 19:35:37 +0900

    메인 중간 사이즈 포스터 이미지 크롤링 및 url도 크롤링

--- Notflix/main/main.py
+++ Notflix/main/main.py
@@ -1,5 +1,6 @@
 # from main import * : main에 선언된 모든 값을 가져온다 , __init__ file에 선언된 라이브러리를 가져와 사용할 수 있음.
 
+from operator import le
 from urllib import response
 
 
@@ -24,34 +25,50 @@ headers = {
 #     'https://movie.naver.com/movie/running/current.naver', headers=headers)
 
 url = 'https://movie.naver.com/movie/running/current.naver'
+
 url2 = 'https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q=%EC%98%81%ED%99%94+%EC%88%9C%EC%9C%84'
 blueprint = Blueprint("main", __name__, url_prefix="/")
 response = requests.get(url2)
+
 soup = BeautifulSoup(response.content, 'html.parser')
+movieInfoList = soup.find('ol', attrs={'class': 'movie_list'}).find_all('li')
+for movieInfo in movieInfoList:
+    movieImg = movieInfo.find('img').attrs['src']
+    movieLink =movieInfo.find('a').attrs.get('href')
+    movieTitle=movieInfo.find('a',attrs={'class':'tit_main'})
+    # print(f'이미지: {movieImg}')
+    doc = {
+        'movieLink': "https://search.daum.net/search?"+movieLink,
+        'movieImg': movieImg,
+        'movieTitle':movieTitle.get_text()
+    }
+   
+    # print("https://search.daum.net/search?"+movieLink)
+   
+    
+    
+    # db.mainMovie.insert_one(doc)
+    
+    
+    
 
-
-# movieInfoList = soup.find('ol', attrs={'class': 'movie_list'}).find_all('li')
-# for movieInfo in movieInfoList:
-#     movieImg = movieInfo.find('img').attrs.get('src')
-
-#     # print(f'이미지: {movieImg}')
-#     doc = {
-#         'movieImg': movieImg
-#     }
-# db.mainMovie.insert_one(doc)
 
 
 # ul = soup.find('ul', class_="lst_detail_t1")
 # images = ul.select('li> div > a >img')
+# movies_names=ul.select('li > dl> dt >a')
 # print(len(images))
 
-# for image in images:
-
+# for image in images:  
 #     doc = {
-#         'movieImg': image['src']
-#     }
+#                 'movieImg': image['src']
+#             }
+#     db.MainMoviesPage.insert_one(doc)
+#     db.MainMoviesPage.drop()
+  
+    
 
-# db.mainMoviePage1.insert_one(doc)
+    
 
 
 @ blueprint.route("/")  # <- 데코레이터
@@ -69,24 +86,32 @@ def show_main_movies():
     naverMovie_List = list(db.main.find({}, {'_id': False}))
     return jsonify({'naverMovies': naverMovie_List})
 
-
+# 완전 큰 메인 
 @blueprint.route("/showDaumMovie", methods=["GET"])
 def show_main_daum_movies():
     daumMovie_List = list(db.MainPageImg.find({}, {'_id': False}))
     return jsonify({'daumMovies': daumMovie_List})
 
 
-@blueprint.route("/MainPagemovie",methods=["POST"])
-def check_post():
+#  중간 크기 메인 
+@blueprint.route("/showDaumMovieMain", methods=["GET"])
+def show_main_daum_movies_middle():
+    daumMovieMain_List = list(db.mainMovie.find({}, {'_id': False}))
+    return jsonify({'daumMoviesMain': daumMovieMain_List})
 
-    url_receive = request.form['url_give']
-    Movie_receive=request.form['Movieurl__give']
 
-    doc={
-            'Imageurl':url_receive,
-            'AdressUrl':Movie_receive
-        }
-    db.MainPageImg.insert_one(doc)
+# 완전 큰 메인 입력할 때 씀 
+# @blueprint.route("/MainPagemovie",methods=["POST"])
+# def check_post():
+
+#     url_receive = request.form['url_give']
+#     Movie_receive=request.form['Movieurl__give']
+
+#     doc={
+#             'Imageurl':url_receive,
+#             'AdressUrl':Movie_receive
+#         }
+#     db.MainPageImg.insert_one(doc)
         
 
    
--- Notflix/main/static/main.js
+++ Notflix/main/static/main.js
@@ -1,3 +1,5 @@
+
+
 const nav=document.getElementById('nav');
 
 window.addEventListener('scroll',()=>{
@@ -12,11 +14,12 @@ window.addEventListener('scroll',()=>{
 
 $(document).ready(function () {
     showNaverMovies()
-    showDaumMovies()
+    showDaumMovie()
+    showDaumMoviesMain()
 
 });
 
-// 메인 페이지에서 네이버 상영작 가져오기 
+// 메인 페이지에서 네이버 상영작(작은 포스터) 가져오기 
 function showNaverMovies(){
     
         $.ajax({
@@ -36,8 +39,31 @@ function showNaverMovies(){
         });
     
 }
+// 중간 크기 메인 
+function showDaumMoviesMain(){
+    $.ajax({
+        type: "GET",
+        url: "/showDaumMovieMain",
+        data: {},
+        success: function (response) {
+            let rows = response['daumMoviesMain']
 
-function showDaumMovies(){
+            for(let i=0; i<rows.length; i++){
+                let movieLink=rows[i]['movieLink']
+                let movieImg=rows[i]['movieImg']
+                let temp_html=`<a href=${movieLink} class="row__poster row__posterLarge"><img src=${movieImg} ></a>`              
+           
+                $('#row_posters_middle').append(temp_html)
+        }
+    }
+    });
+}
+new Carousel(document.querySelector('#carousel-banner'));
+
+
+
+// 완전 메인 포스터 
+function showDaumMovie(){
     $.ajax({
         type: "GET",
         url: "/showDaumMovie",
@@ -48,12 +74,16 @@ function showDaumMovies(){
             for(let i=0; i<rows.length; i++){
                 let image=rows[i]['Imageurl']
                 let MimageAddress=rows[i]['AdressUrl']
-                let temp_html=`
-                <div class=" banner__contents carousel-item">
-                <a href=${MimageAddress}><img src=${image} ></a>
-               </div>`              
-           
+                
+                let temp_html=            
+               `<div class= "banner__contents carousel-item"> 
+                 <a href=${MimageAddress} ><img src=${image} ></a>
+               </div>`
+                           
+                      
                 $('#carouselExampleControls').append(temp_html)
+                
+                
         }
     }
     });
@@ -77,6 +107,12 @@ const swiper = new Swiper('.swiper', {
  
   });
 
+ 
+
+
+
+  
+  
 // 메인에 넣을 이미지 주소 db에 넣기 
   function posting() {
     let url = $('#url').val()
