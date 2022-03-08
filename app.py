4e732918eb327b223726aa1a5e5caf255f1a1981
Devlee <hjcloud@me.com>
2022-03-08 15:24:24 +0900

    first JWT

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from http import client
 from flask import Flask, flash, render_template, request, url_for, jsonify, redirect, session
 from pymongo import MongoClient
+import jwt
+import datetime
 import hashlib
 app = Flask(__name__)
+
+SECRET_KEY = 'GAZUAA'
 
 import certifi
 ca = certifi.where()
@@ -21,6 +25,35 @@ def mypage_template():
 @app.route("/login/")
 def login_template():
     return render_template("login.html")
+
+@app.route("/login/done")
+def done_template():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    # 회원가입 때와 같은 방법으로 pw를 암호화합니다.
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+
+    # id, 암호화된pw을 가지고 해당 유저를 찾습니다.
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+
+    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    if result is not None:
+        # JWT 토큰에는, payload와 시크릿키가 필요합니다.
+        # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있습니다.
+        # 아래에선 id와 exp를 담았습니다. 즉, JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # exp에는 만료시간을 넣어줍니다. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 납니다.
+        payload = {
+            'id': id_receive,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        # token을 줍니다.
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route("/join")
 def join_template():
--- static/login.js
+++ static/login.js
@@ -15,3 +15,24 @@ $(btn).on('click', function() {
         },1500);
     }
 })
+
+function login() {
+    $.ajax({
+        type: "POST",
+        url: "/login/done",
+        data: {id_give: $('#id').val(), pw_give: $('#pw').val()},
+        success: function (response) {
+            if (response['result'] == 'success') {
+                // 로그인이 정상적으로 되면, 토큰을 받아옵니다.
+                // 이 토큰을 mytoken이라는 키 값으로 쿠키에 저장합니다.
+                $.cookie('mytoken', response['token']);
+
+                alert('로그인 완료!')
+                window.location.href = '/'
+            } else {
+                // 로그인이 안되면 에러메시지를 띄웁니다.
+                alert(response['msg'])
+            }
+        }
+    })
+}
